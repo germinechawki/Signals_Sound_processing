7ca9544870abcf3f84d81538db694a82dae5a04d
YoustinaZak <125680172+YoustinaZak@users.noreply.github.com>
2023-12-26 20:06:40 +0000

    Start

--- Python_code.py
+++ Python_code.py
@@ -1,1 +1,15 @@
-print ("hello zakziko")
+#function of accessing the audio file
+
+#function of using fourier
+
+#plot freq and time before
+
+#plot freq and time after
+
+#transform
+
+#apply filter
+
+#transform
+
+#function of saving new audio
