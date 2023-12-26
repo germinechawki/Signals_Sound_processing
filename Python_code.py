b51a2d2ff43c75d452da091587366c8878824275
Mark Magdy <mark.anis12@gmail>
2023-12-26 22:55:16 +0200

    functions to start

--- Python_code.py
+++ Python_code.py
@@ -1,15 +1,51 @@
-#function of accessing the audio file
+# function of accessing (reading) the audio file
+def read_file():
+    # write your code here "" :)
+    return
 
-#function of using fourier
 
-#plot freq and time before
+# plot audio file (time domain) before editing
+def plt_time_domain_before():
+    # write your code here "" :)
+    return
 
-#plot freq and time after
 
-#transform
+# function of using fourier transform
+def fourier_transform():
+    # write your code here "" :)
+    return
 
-#apply filter
 
-#transform
+# plot audio file (freq domain) before editing
+def plt_freq_domain_before():
+    # write your code here "" :)
+    return
 
-#function of saving new audio
+
+# apply filter lowpass and high pass filter
+def filter_signal():
+    return
+
+
+# plot audio file (freq domain) after editing
+def plt_freq_domain_after():
+    # write your code here "" :)
+    return
+
+
+# function of using inverse fourier transform
+def fourier_inverse_transform():
+    # write your code here "" :)
+    return
+
+
+# plot audio file (time domain) after editing
+def plt_time_domain_after():
+    # write your code here "" :)
+    return
+
+
+# function of saving new audio
+def save_file():
+    # write your code here "" :)
+    return
